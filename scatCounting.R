2b619c15d07b7333977a0e34b94687061d7e02e3
Alec Wong <aw685@cornell.edu>
2018-02-18 13:15:42 -0500

    Reformatted script to accommodate functions.R

--- scatCounting.R
+++ scatCounting.R
@@ -12,50 +12,17 @@ library(dplyr)
 library(foreach)
 library(broom)
 
+source('functions.R')
+
 # Format example dog tracks ------------------------------------------------------------------------------------------------------------
 
 library(rgdal)
 
+# These are the sites that the gpx files are pulled from. Can probably automate in the future if needed. 
+
+# Press F2 on any function to see its contents.
+
 sites = c(rep("12B2",3), rep("15A4", 3))
-
-getGPX = function(){
-  
-  gpxFiles = dir()[grep(pattern = '.gpx', x = dir(), perl = T)]
-  
-  gpxLayers = ogrListLayers(gpxFiles[1])
-  
-  if(!exists("out", where = .GlobalEnv)){
-    out = lapply(X = gpxFiles, FUN = function(x){readOGR(dsn = x, layer = 'track_points')})
-  }
-  
-  names(out) = gpxFiles
-  
-  # Convert to UTM for easy grid creation.
-  
-  out = lapply(X = out, FUN = function(x) spTransform(x = x, CRSobj = CRS("+proj=utm +zone=18 +datum=WGS84")))
-  
-  return(out)
-}
-
-convertPoints = function(){
-
-  allPoints = foreach(i = seq_along(out), .combine = rbind) %do% {
-    data.frame(out[[i]]@coords, Site = sites[i], Date = out[[i]]@data$time %>% as.Date %>% unique) %>% rename(Easting = coords.x1, Northing = coords.x2)
-  }
-  
-  allPoints$Date = as.factor(allPoints$Date)
-  
-  # Set dates to "round" equivalents.
-  
-  allPoints = allPoints %>% mutate(Round = factor(ifelse(test = Date %in% c("2017-07-07", "2017-07-17"), yes = "Round 1", 
-                                                         ifelse(test = Date %in% c("2017-07-30", "2017-07-28"), yes = "Round 2", no = "Round 3"))),
-                                   RoundBySite = interaction(Site, Round))
-  
-  coordinates(allPoints) = ~Easting + Northing
-  
-  return(allPoints)
-  
-}
 
 out = getGPX()
 
@@ -90,36 +57,7 @@ ylim = c(-2,2)
 
 # Generate grid
 
-getGrid = function(adj.bbox = 100){
-  
-  allPoints_bt = apply(X = allPoints@coords, MARGIN = 1, FUN = function(x){x*scaled_12B2_scale + scaled_12B2_center}) %>% t %>% data.frame
-  
-  coordinates(allPoints_bt) = ~Easting + Northing
-  
-  (bbox = allPoints_bt@bbox)
-  
-  bbox = bbox + c(rep(-adj.bbox, 2), rep(adj.bbox, 2))
-  
-  gridOverlay = expand.grid(seq(bbox[1,1], bbox[1,2], 50), seq(bbox[2,1], bbox[2,2], 50))
-  
-  scaledGrid = gridOverlay  %>% scale(center = T, scale = meanScale) %>% as.data.frame %>% rename(Easting = Var1, Northing = Var2)
-  
-  return(scaledGrid)
-  
-}
-
 scaledGrid = getGrid()
-
-getBbox = function(grid){
-  
-  mins = c(min(grid[,1]), min(grid[,2]))
-  maxs = c(max(grid[,1]), max(grid[,2]))
-  
-  data = cbind(mins, maxs)
-  
-  return(data)
-  
-}
 
 bbox_scaled = getBbox(scaledGrid)
 
