cef5da18ec3a6eae09d16510906de1f98b4a4717
Unknown <aw685@cornell.edu>
2018-07-23 18:43:13 -0400

    dataFormatting.R : lots of comments, some exploring

--- dataFormatting.R
+++ dataFormatting.R
@@ -142,9 +142,10 @@ if(!skip){
   trackIntervals$out %>% lapply(FUN = function(x){summary(x %>% as.integer)})
 }
 
-# How many replicated observations? oof . . . 26 replicated visits out of 5
+# How many replicated observations? oof . . . 27 replicated visits out of 5
 
 rleScats %>% lapply(FUN = function(x){x$values %>% table %>% as.integer}) %>% unlist %>% table
+rleTracks %>% lapply(FUN = function(x){x$values %>% table %>% as.integer}) %>% unlist %>% table
 
 # Look at 266; 9A1.Sample1
 
@@ -164,4 +165,59 @@ ggplot() +
 
 
 # Format as needed by model ------------------------------------------------------------------------------------------------------------------------------------------------
+
+# What we need: 
+# * y[i,t,v]
+# * vis[i,t,v]
+# * days[i,t-1]
+
+# So what we're looking at, mainly, is a model of per-grid-cell initial deposition
+# (lambda), per-grid-cell daily deposition (theta), probability of detection
+# (p).
+
+# There is a matrix of scats available to be sampled N. It is of dimension N[i,t,v], where 
+# i = grid cell index
+# t = index for sampling round (0th, 1st, 2nd, 3rd). There is no sampling performed in the 0th time period. The clearing round is, then, round 1, indexed by t = 2
+# v = index for replicated observation of grid cell i
+# This is unobserved, but must be initialized (?)
+
+# There is a matrix of observations y[i,t,v] such that y[i,t,v] ~ Bin(N[i,t,v], p) , at least for the homogeneous p variety.
+# y[1,t,v] is all 0's. If there are four sampling occasions max, maxT = 5. 
+# Say a scat was found in a grid cell i, then another upon return to grid cell i, during time t. The structure would be y[i,t,1:2] = 1, and y[i,t,3:maxV] = 0
+# I plan to start with a huge matrix of all grid cell ID's i, then cut out the indexes that don't matter (singleVisit sites, or no observations), then re-index, saving this index for reference as a data frame.
+
+# There is a matrix of depositions R[i,t], which is applied after sampling is performed such that 
+# N[i,t,1] = N[i,t-1,2] - y[i,t-1,2] + R[i,t-1]   :  Meaning, at the start of a new sampling round, what is there depends on what was there after 
+# N[i,t,2] = N[i,t,1] - y[i,t,1]
+
+# There is a matrix of visitation, indicating whether grid cells i were visited in round t, with replicate v; all 1's or 0's. If the grid cell was not visited, then p(observation) = 0 by way of multiplication: p[i,t,v] = p0[i,t,v] * vis[i,t,v]. P(observation) in t = 1 (initial deposition) is 0. 
+
+# Get y from rleScats --------------------------------------------------------------------------------------------------------------------------------
+
+# Why am I using tracks in the next line? Well, I need to format y such that, if a grid cell was visited 20 times
+
+maxV = rleTracks %>% lapply(FUN = function(x){x$values %>% table %>% as.integer}) %>% unlist %>% max 
+
+# Skip
+
+if(!skip){
+
+    # 9A1.Clearing has a grid cell that was visited 20 times...what does this look like?
+  rleTracks %>% lapply(FUN = function(x){x$values %>% table %>% as.integer}) %>% sapply(X = ., FUN = function(x){any(x == 20)}) %>% {which(.)}
+  
+  grid_local = grids[[which(siteOrder == '09A1')]]
+  scats_local = scatsReferenced_spdf[scatsReferenced$RndBySt == '09A1.Clearing',]
+  
+  # It looks like that tough part in the left hand side.
+  ggplot() + 
+    geom_tile(data = grid_local %>% data.frame, aes(x = x, y = y), fill = NA, color = 'black') + 
+    geom_text(data = grid_local %>% data.frame, aes(x = x, y = y, label = id), color = 'gray50', size = 3) + 
+    geom_point(data = tracks2016_points %>% data.frame %>% filter(RndBySt == '09A1.Clearing'), aes(x = Easting, y = Northing), shape = 1) +
+    geom_point(data = scats_local %>% data.frame, aes(x = Easting, y = Northing, color = 'red')) + 
+    coord_equal(xlim = c(min(scats_local$Easting) + c(-100,100), max(scats_local$Easting) + c(-100,100)), 
+                ylim = c(min(scats_local$Northing) + c(-100,100), max(scats_local$Northing) + c(-100,100)))
+  
+  # End skip
+}
+
 
