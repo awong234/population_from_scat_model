d7fac392395c2f76c897ee5d46842ec8657a1fb5
Unknown <aw685@cornell.edu>
2018-07-23 12:37:41 -0400

    dataFormatting.R : import most up-to-date data

--- dataFormatting.R
+++ dataFormatting.R
@@ -7,11 +7,17 @@ source('functions.R')
 
 # Import data ------------------------------------------------------------
 
-tracks2016 = rgdal::readOGR(dsn = 'gpxTracks2016_CLEANED', layer = 'gpxTracks2016_CLEANED')
+tracks2016 = rgdal::readOGR(dsn = 'gpxTracks2016_CLEANED', layer = 'tracks2016_clean')
+
+tracks2016_points = rgdal::readOGR(dsn = 'gpxTracks2016_points_CLEANED', layer = 'tracks2016_points_clean', stringsAsFactors = F)
+
 scats2016 = read.csv(file = 'scatLocs2016_cleaned.csv')
 
-tracks2016_points = as(object = tracks2016, "SpatialPointsDataFrame")
+load('scatsData.Rdata')
+
 attr(tracks2016_points@coords, 'dimnames') = list(NULL, c("Easting", "Northing"))
+
+# Make grid ------------------------------------------------------------------------------------------------------------------------
 
 # Make a grid over each of the tracks
 
@@ -21,8 +27,6 @@ siteOrder = tracks2016@data$Site %>% unique %>% as.character()
 # Look at a grid and a corresponding track
 
 siteIndexes = which(tracks2016@data$Site == '12B2')
-
-i = 1
 
 track_12B2 = tracks2016_points[which(tracks2016_points@data$Site == '12B2'),]
 
@@ -37,8 +41,6 @@ ggplot() +
 
 # Can we identify cells crossed by tracks?
 
-?over
-
 intersect = rle(over(x = track_12B2, y = grid_12B2)$id)
 
 intersect$values
@@ -48,7 +50,8 @@ ggplot() +
   geom_tile(data = grid_12B2 %>% data.frame %>% filter(id %in% intersect$values), aes(x = x, y = y), color = 'red', fill = NA) +
   geom_text(data = grid_12B2 %>% data.frame, aes(x = x, y = y, label = id), size = 1) +
   geom_point(data = track_12B2 %>% data.frame, aes(x = Easting, y = Northing)) + 
-  geom_point(data = scats2016 %>% filter(Site == '12B2'), aes(x = Easting, y = Northing), shape = 1, color = 'blue') + 
+  geom_point(data = scatsReferenced %>% filter(Site == '12B2'), aes(x = Easting, y = Northing), shape = 1, color = 'blue') + 
+  geom_point(data = scats2016 %>% filter(Site == '12B2'), aes(x = Easting, y = Northing), shape = 1, color = 'green') + 
   coord_equal()
 
 # Yes, looks good. We also get the order of visitation from intersect$values.
@@ -59,27 +62,10 @@ allIDs = foreach(i = 1:length(grids), .combine = c) %do% {
   grid$id
 }
 
-# Shoot, we need the scats to be temporally referenced as well. Need to get a) scat collection time and b) nearest transect point in space & time, and assign that transect ID to it.
-# scat data still temporally referenced, but track time data was lost in cleaning. Have to hope that the times were aligned.
+identical(allIDs, seq_along(allIDs))
 
-load('trackPoints_2016_unclean.Rdata')
+# Get records of visitations to each grid cell, by ID, and the quantity of scats picked up -----------------------------------------------------------
 
-# For EVERY track point, we need to find the closest one, and then inherit the time from it. 
 
-registerDoParallel(cores = detectCores())
 
-tracks2016_points@data$Date_Time = NA
-
-foreach(i = 1:nrow(tracks2016_points)) %do% {
-  
-  site = tracks2016_points[i,]$Site %>% as.character
-  
-  tracksFromSite = tracks_points[which(tracks_points$Site == site),]
-  
-  dists = rgeos::gDistance(tracks2016_points[i,], tracksFromSite, byid = T)
-  
-  index = which.min(dists)
-  
-  tracks2016_points@data$Date_Time[i] = tracksFromSite[index,]$Time
-  
-}
+# Format as needed by model ------------------------------------------------------------------------------------------------------------------------------------------------
